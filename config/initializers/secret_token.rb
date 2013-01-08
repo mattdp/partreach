6859cc7f61aeb80b9df41d26b08198f111f34ca5
Matthew Du Pont <dupont@okcupidlabs.com>
2013-01-07 21:52:52 -0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Partreach::Application.config.secret_token = '888cf26c58c3b252b333d853910d07d8e3b695345b62cf1a4898ccc5d83cddcf834d2f96c1e04e41cec100bea1b4ebb759b319b4917c9a323344681039af82ad'
