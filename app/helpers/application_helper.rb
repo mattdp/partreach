fc7aab6b1cbf9d865bfa2be0c3d4a1be9c58931a
Matthew Du Pont <dupont@okcupidlabs.com>
2013-01-08 20:55:01 -0800

    link to external urls

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+
+	def url_with_protocol(url) #from http://stackoverflow.com/questions/5012188/rails-link-to-external-site-url-is-attribute-of-user-table-like-users-websit
+    /^http/.match(url) ? url : "http://#{url}"
+  end
+
 end
