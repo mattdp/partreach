e75b7d23df7654453817e078e1edbade83398c0c
Matthew Du Pont <dupont@okcupidlabs.com>
2013-01-19 20:17:01 -0800

    user passwords and confirmations, and tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,7 +12,8 @@
 #
 
 class User < ActiveRecord::Base
-  attr_accessible :admin, :email, :name
+  attr_accessible :admin, :email, :name, :password, :password_confirmation
+  has_secure_password
 
   before_save { |user| user.email = email.downcase }
 
@@ -23,4 +24,7 @@ class User < ActiveRecord::Base
   validates :name, presence: true, length: { maximum: 20 }, uniqueness: true
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates	:email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
+  validates :password, presence: true, length: { minimum: 6 }
+  validates :password_confirmation, presence: true
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130120031758) do
+ActiveRecord::Schema.define(:version => 20130120040750) do
 
   create_table "addresses", :force => true do |t|
     t.string   "name"
@@ -55,9 +55,10 @@ ActiveRecord::Schema.define(:version => 20130120031758) do
     t.string   "name"
     t.string   "email"
     t.boolean  "admin"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at",      :null => false
+    t.datetime "updated_at",      :null => false
     t.integer  "address_id"
+    t.string   "password_digest"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
