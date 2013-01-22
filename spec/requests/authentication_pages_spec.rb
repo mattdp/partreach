78c7538e18bc610504a179008867df78ed82280b
Matthew Du Pont <dupont@okcupidlabs.com>
2013-01-22 06:48:15 -0800

    protect index, add faker

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -49,6 +49,12 @@ describe "Authentication" do
           before { put user_path(user) }
           specify { response.should redirect_to(signin_path) }
         end
+
+        describe "visiting the user index" do
+          before { visit users_path }
+          it { should have_selector('h1', text: 'Sign in') }
+        end
+
       end
 
       describe "when attempting to visit a protected page" do
