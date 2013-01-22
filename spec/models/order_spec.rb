8a3474d39fb0048d820bb45bdf470ffcea52b2b1
Matthew Du Pont <dupont@okcupidlabs.com>
2013-01-22 07:34:54 -0800

    few more tests to do

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -2,4 +2,6 @@
 #quantity exists
 #quantity is above 0
 #can't hit id via mass assignment
-#destroying user destroys order
+#destroying user destroys order (request)
+#can only see one's own orders (request)
+#admin can see all orders (request)
