ac9bae4704b806bd2676e4cb959e333d10bf09ff
Matthew Du Pont <dupont@okcupidlabs.com>
2013-02-02 10:02:43 -0800

    eliminating association test

--- spec/requests/orders_and_user_pages_spec.rb
+++ spec/requests/orders_and_user_pages_spec.rb
@@ -3,32 +3,18 @@ require 'spec_helper'
 describe "Order and users" do
 
 	#paralell processes to build these; somewhat brittle, but learned a bunch about factories doing it
-	built_orders = FactoryGirl.build_list(:order, 2)
-	built_users =  FactoryGirl.build_list(:user, 2)
+	created_orders = FactoryGirl.create_list(:order, 2)
+	created_users =  FactoryGirl.create_list(:user, 2)
 
 	describe "basic factory sanity" do # PICK UP HERE
+
 		it "should have the order attached to the correct user" do
-			built_orders[0].user.id == built_users[0].id
+			created_orders[0].user.id == created_users[0].id
 		end
 
 		it "shouldn't have the order attached to the wrong user" do
-			built_orders[1].user.id != built_users[0].id
+			created_orders[1].user.id != created_users[0].id
 		end
-	end
-
-	describe "destruction: ", :broken => true do
-		o = FactoryGirl.create(:order)
-		u = o.user
-
-		describe "order should exist before user is destroyed" do
-			it { expect { o.quantity }.to_not raise_error }
-		end
-
-		describe "order shouldn't exist after user is destroyed" do
-			before { u.destroy }
-			it { expect { o.quantity }.to raise_error }
-		end
-
 	end
 
 	describe "lack of orders" do
