2ba05c1d9e9b44b2e83e951e6656fd1929886f23
mattdp <matthew.frank.dupont@gmail.com>
2013-03-29 17:51:45 -0700

    erb table

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -9,6 +9,12 @@ class StaticPagesController < ApplicationController
 	end
 
 	def materials
+		@data_by_process = [
+				{process: "SLS", material: "Nylon", looking_for: ["Good for mechanical performance","Good for mass production"], downsides: ["Worse than FDM for accuracy"]},
+				{process: "SLA", material: "UV Curable Resin", looking_for: ["Application flexible","Good for mass production", "Great resolution"], downsides: ["Poor for mechanical performance"]},
+				{process: "FDM", material: "ABS, PLA", looking_for: ["Great for accuracy", "Great for mechanical performance"], downsides: ["Poor for mass production"]},
+				{process: "Polyjet", material: "ABS-like", looking_for: ["Great for accuracy", "Great for prototyping"], downsides: ["Poor for production", "Poor for mechanical performance"]}
+				]
 	end
 	  
 end
