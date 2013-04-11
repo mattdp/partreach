c9665576cdce541353f99caa4829a0faeaf6f53f
mattdp <matthew.frank.dupont@gmail.com>
2013-04-11 12:33:39 -0700

    can add a dialogue

--- app/controllers/dialogues_controller.rb
+++ app/controllers/dialogues_controller.rb
@@ -1,29 +1,28 @@
 class DialoguesController < ApplicationController
+	before_filter :admin_user, only: [:new, :create]
 
 	def new
-		@dialogue = Dialogue.new
-		#@order = Order.find(params[:id])
 		@suppliers = Supplier.all
-
-		respond_to do |format|
-			format.html
-      format.json { render json: @dialogue }
-    end
 	end
 
 	def create
 
-		@order = Order.find(params[:id])
+		@order = Order.find(params[:order_id_field])
+		@supplier = Supplier.find(params[:supplier_selection])
 
-		did_dialogue_save = true
+		@dialogue = Dialogue.new
+		@dialogue.order_id = @order.id
+		@dialogue.supplier_id = @supplier.id
 
-		if did_dialogue_save
-			format.html { redirect_to @order, notice: 'Dialogue added to order.' }
-      format.json { render json: @order, status: :created, location: @order }
-		else 
-			format.html { render action: "new" }
-      format.json { render json: @dialogue.errors.full_messages, status: 400 }
-    end
+		respond_to do |format|
+			if @dialogue.save
+				format.html { redirect_to "/orders/manipulate_dialogues/#{@dialogue.order_id}", notice: 'Dialogue added to order.' }
+	      format.json { render json: @order, status: :created, location: @order }
+			else 
+				format.html { render action: "new" }
+	      format.json { render json: @dialogue.errors.full_messages, status: 400 }
+	    end
+	  end
   end
 
 end
