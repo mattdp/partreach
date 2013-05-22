7215f359343cca31f0b1e0647132d7f34e844190
Robert Martinez <robert.david.martinez@gmail.com>
2013-05-22 14:23:33 -0700

    copy text changes and adding a column in the table

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -10,10 +10,10 @@ class StaticPagesController < ApplicationController
 
 	def materials
 		@data_by_process = [
-				{process: "SLS", material: "Nylon", looking_for: ["Good mechanical performance","Mass production capability"], downsides: ["Worse accuracy than FDM"]},
-				{process: "SLA", material: "UV Curable Resin", looking_for: ["Flexible applications","Mass production ", "Great resolution"], downsides: ["Poor mechanical performance"]},
-				{process: "FDM", material: "ABS, PLA", looking_for: ["Great accuracy", "Great mechanical performance"], downsides: ["Poor for mass production"]},
-				{process: "Polyjet", material: "ABS-like", looking_for: ["Great accuracy", "Great prototyping"], downsides: ["Poor for production", "Poor mechanical performance"]}
+				{process: "SLS", material: "Nylon", looking_for: ["Decent mechanical performance","Low-volume production"], downsides: ["Worse accuracy than FDM", "Poor for closed hollow geometries"], example: ["A run of 20 custom cable brackets"]},
+				{process: "SLA", material: "UV Curable Resin", looking_for: ["Flexible applications", "Great resolution"], downsides: ["Poor mechanical performance"], example: ["A master print for Rapid Casting"]},
+				{process: "FDM", material: "ABS, PLA", looking_for: ["Great mechanical performance"], downsides: ["Poor for mass production"], example: ["A pair of large hollow wheels"]},
+				{process: "Polyjet", material: "ABS-like", looking_for: ["Great accuracy", "Can mix materials"], downsides: ["Poor mechanical performance over time"], example: ["A 'looks-like' prototype of a new toothbrush design"]}
 				]
 	end
 	  
