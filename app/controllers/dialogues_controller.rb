1998aac473334cf3a0c28c760e031ca7f987d4ba
mattdp <matthew.frank.dupont@gmail.com>
2013-05-27 23:24:23 -0400

    buggy, working mass tag updates

--- app/controllers/dialogues_controller.rb
+++ app/controllers/dialogues_controller.rb
@@ -9,29 +9,47 @@ class DialoguesController < ApplicationController
 	def create
 
 		saved_ok = true
+		@order = Order.find(params[:order_id_field])
+		@supplier_ids = params[:supplier_selection]
+		@tag_ids = params[:tag_selection]
 
 		if params[:form_use] == "add_dialogues"
-			@order = Order.find(params[:order_id_field])
-			@supplier_ids = params[:supplier_selection]
 
 			@supplier_ids.each do |s|
 
 				@dialogue = Dialogue.new
 				@dialogue.order_id = @order.id
 				@dialogue.supplier_id = s.to_i
-				if !@dialogue.save
+				if !@dialogue.save #replace with unless
 					saved_ok = false
 				end
 			end
+			redir_to = "/orders/manipulate_dialogues/#{@dialogue.order_id}"
+			redir_notice = 'Dialogue added to order.'
+
 		elsif params[:form_use] == "add_tags"
-			#tag logic
+			
+			@supplier_ids.each do |s|
+				@tag_ids.each do |t|
+
+					@combo = Combo.new
+					@combo.supplier_id = s
+					@combo.tag_id = t
+					if !@combo.save
+						saved_ok = false
+					end
+				end
+			end
+			redir_to = "/orders"
+			redir_notice = 'Tags added to suppliers.'
+
 		else #should never happen
 			saved_ok = false 
 		end	
 
 		respond_to do |format|
 			if saved_ok
-				format.html { redirect_to "/orders/manipulate_dialogues/#{@dialogue.order_id}", notice: 'Dialogue added to order.' }
+				format.html { redirect_to redir_to, notice: redir_notice }
 	      format.json { render json: @order, status: :created, location: @order }
 			else 
 				format.html { render action: "new" }
