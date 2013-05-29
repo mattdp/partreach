0320fdede984a27cd9eae4c247feb199eefb3b9a
mattdp <matthew.frank.dupont@gmail.com>
2013-05-28 21:34:55 -0700

    attempt to fix precompile

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,10 @@ end
 
 module Partreach
   class Application < Rails::Application
+
+    #based on https://devcenter.heroku.com/articles/rails-asset-pipeline
+    config.assets.initialize_on_precompile = false
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
