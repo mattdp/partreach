3d40ae5cf8924021727693e66fe935f3b1cfd87a
mattdp <matthew.frank.dupont@gmail.com>
2013-06-09 15:39:22 -0700

    tabs, but no text, for bottom of profile

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,6 +1,11 @@
 class ProfilesController < ApplicationController
 
 	def supplier_profile
+		# toggle if certain parts of the profile are visible
+		@machines_toggle = true
+		@reviews_toggle = true
+		@photos_toggle = true
+
 		@supplier = Supplier.where("lower(name) = ?", params[:name].downcase).first
 	end
 
