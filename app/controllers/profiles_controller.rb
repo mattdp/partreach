27dc394610e9de0e68cfc9a10ab7cddaad006b3d
mattdp <matthew.frank.dupont@gmail.com>
2013-06-11 09:10:59 -0700

    asks for information not yet had

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -6,13 +6,19 @@ class ProfilesController < ApplicationController
 		@reviews_toggle = true
 		@photos_toggle = true
 
+		current_user.nil? ? @user_id = 0 : @user_id = current_user.id
+
 		@supplier = Supplier.where("lower(name) = ?", params[:name].downcase).first
 	end
 
 	def submit_ask
 		#create ask based on inbound information (where get?)
+		a = Ask.new(supplier_id: params[:supplier_id], request: params[:request], user_id: params[:user_id])
+		a.save
 		#flash message of request received
 		#route to that supplier profile
+		@supplier = Supplier.find(params[:supplier_id])
+		redirect_to supplier_profile_url(@supplier.name), notice: 'Suggestion received. Thanks!'
 	end
 
 end
