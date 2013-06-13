d45c9c46ec0afdc25460d8a22742624e899fd30c
mattdp <matthew.frank.dupont@gmail.com>
2013-06-13 12:59:32 -0700

    working search

--- app/assets/javascripts/dialogues.js
+++ app/assets/javascripts/dialogues.js
@@ -1,5 +1,14 @@
 $(document).ready(function() {
-	$("#all-suppliers").click(function() {
-		$(".supplier-checkbox").attr("checked","true")
+
+	$('#all-suppliers').click(function() {
+		$('.supplier-checkbox').attr("checked","true")
 	})
+	
+	$('#tag_matrix').find('input').change(function() {
+		var tag_name = $(this).attr('id');
+		if($('#add-dialogues-radio').is(':checked')) {
+			$('#supplier_matrix').find('input').not('.'+tag_name).closest('td').toggleClass('hidden');
+		}
+	})
+
 })
