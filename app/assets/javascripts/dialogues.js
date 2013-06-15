1f6d65d807297d426e94f1841257096e07cf1f8a
mattdp <matthew.frank.dupont@gmail.com>
2013-06-15 12:23:17 -0700

    multitag search

--- app/assets/javascripts/dialogues.js
+++ app/assets/javascripts/dialogues.js
@@ -1,13 +1,40 @@
 $(document).ready(function() {
 
+	//select all suppliers
 	$('#all-suppliers').click(function() {
 		$('.supplier-checkbox').attr("checked","true")
 	});
-	
+
+	//allow multitag searching
 	$('#tag_matrix').find('input').change(function() {
-		var tag_name = $(this).attr('id');
 		if($('#add-dialogues-radio').is(':checked')) {
-			$('#supplier_matrix').find('input').not('.'+tag_name).closest('td').toggleClass('hidden');
+			$('#supplier_matrix').find('td').addClass('hidden');
+
+			//get a list of all checked boxes
+			var checkedBoxes = [];
+			$('#tag_matrix').find('input').each (function () {
+				if($(this).is(':checked')) {
+					checkedBoxes.push($(this).closest('td').find('p').text());
+				}
+			});
+
+			var activeCheckbox = true;
+			var unhide = true;
+			var i = 0;
+
+			//reveal all that have each criterion
+			$('#supplier_matrix').find('input').each (function () {
+				activeCheckbox = $(this);
+				unhide = true;
+				for (i = 0; i < checkedBoxes.length; i = i + 1) {
+					if (!activeCheckbox.hasClass(checkedBoxes[i])) {
+						unhide = false;
+					}
+				}
+				if (unhide) {
+					activeCheckbox.closest('td').removeClass('hidden');
+				}
+			});
 		}
 	});
 
