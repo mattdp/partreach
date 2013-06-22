c9e8afbbbe03ba7a5bd79ffac9c1ce1febc594a3
mattdp <matthew.frank.dupont@gmail.com>
2013-06-21 17:54:02 -0700

    no nils in supplier new

--- app/controllers/suppliers_controller.rb
+++ app/controllers/suppliers_controller.rb
@@ -8,11 +8,11 @@ class SuppliersController < ApplicationController
 		@supplier = Supplier.new
 
 		@supplier.name = params[:name]
-		@supplier.url_main = params[:url_main]
+		@supplier.url_main = params[:url_main] if !(params[:url_main].nil? or params[:url_main] == "")
 		@supplier.address = Address.new
-		@supplier.address.country = params[:country]
-		@supplier.address.state = params[:state]
-		@supplier.address.zip = params[:zip]
+		@supplier.address.country = params[:country] if !(params[:country].nil? or params[:country] == "")
+		@supplier.address.state = params[:state] if !(params[:state].nil? or params[:state] == "")
+		@supplier.address.zip = params[:zip] if !(params[:zip].nil? or params[:zip] == "")
 
 		note = ""
 		if @supplier.save and @supplier.address.save
