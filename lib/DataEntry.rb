474a8e41bf699ecbbab78bcc7ff8bbfd2b35557b
mattdp <matthew.frank.dupont@gmail.com>
2013-06-28 18:23:16 -0700

    [] not nil

--- lib/DataEntry.rb
+++ lib/DataEntry.rb
@@ -12,7 +12,7 @@ module DataEntry
 												:zip => row[ZIP_POSTAL],
 												:location_name => row[ZIP_LOCATION_NAME]
 											})
-			l.save if Location.where('zip = ?',l.zip).nil?
+			l.save if Location.where('zip = ?',l.zip) == []
 		end
 		return "Load attempted"
 	end
