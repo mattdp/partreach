3d9cdbf9ed1f105165af0f6bc734b3818c35eb18
mattdp <matthew.frank.dupont@gmail.com>
2013-07-12 14:32:24 -0700

    working email sender

--- app/mailers/blast_mailer.rb
+++ app/mailers/blast_mailer.rb
@@ -2,20 +2,17 @@ class BlastMailer < ActionMailer::Base
 	default from: "noreply@supplybetter.com"
   include SessionsHelper
 
-  def blast_email(email_address,subject,content_name)
-  	@content = content_holder(content_name)
-    attachments.inline['example_1.png'] = File.read('./app/assets/images/matt_headshot.png')
-    attachments.inline['example_2.png'] = File.read('./app/assets/images/matt_headshot.png')
-  	mail(to: email_address, subject: subject)
+  def blast_email(email_address,subject)
+    attachments.inline['pixil3d.jpg'] = File.read('./app/assets/images/profile_pixil3d.jpg')
+    attachments.inline['partsnap.jpg'] = File.read('./app/assets/images/profile_partsnap.jpg')
+  	return mail(to: email_address, subject: subject)
   end
 
-  def content_holder(content_name)
-  	case content_name
-  	when "us_supplier_profiles"
-  		return 
-  	else
-  		return false
-  	end
+  def blast_email_sender(addresses,subject)
+    addresses.each do |a|
+     b = BlastMailer.blast_email(a,subject)
+     b.deliver
+    end
   end
 
 end
