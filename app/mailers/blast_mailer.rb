cceed20e562e539e5d3c28bf7b190328ca74bfdc
mattdp <matthew.frank.dupont@gmail.com>
2013-07-12 11:10:46 -0700

    delete unused images, mailer working for basic images

--- app/mailers/blast_mailer.rb
+++ app/mailers/blast_mailer.rb
@@ -4,13 +4,15 @@ class BlastMailer < ActionMailer::Base
 
   def blast_email(email_address,subject,content_name)
   	@content = content_holder(content_name)
+    attachments.inline['example_1.png'] = File.read('./app/assets/images/matt_headshot.png')
+    attachments.inline['example_2.png'] = File.read('./app/assets/images/matt_headshot.png')
   	mail(to: email_address, subject: subject)
   end
 
   def content_holder(content_name)
   	case content_name
   	when "us_supplier_profiles"
-  		return "Sweet email text here"
+  		return 
   	else
   		return false
   	end
