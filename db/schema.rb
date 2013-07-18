ea2d8b197b1f66d8a78fbd686787462c7493b509
mattdp <matthew.frank.dupont@gmail.com>
2013-07-17 18:35:30 -0700

    admin status false, not nil, by default

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130718001909) do
+ActiveRecord::Schema.define(:version => 20130718013443) do
 
   create_table "addresses", :force => true do |t|
     t.string   "name"
@@ -175,9 +175,9 @@ ActiveRecord::Schema.define(:version => 20130718001909) do
   create_table "users", :force => true do |t|
     t.string   "name"
     t.string   "email"
-    t.boolean  "admin"
-    t.datetime "created_at",                               :null => false
-    t.datetime "updated_at",                               :null => false
+    t.boolean  "admin",                  :default => false
+    t.datetime "created_at",                                :null => false
+    t.datetime "updated_at",                                :null => false
     t.integer  "address_id"
     t.string   "password_digest"
     t.string   "remember_token"
