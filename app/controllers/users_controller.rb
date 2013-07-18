b9e2125ec82a0364c3d3bf80703a7a1b16fb2437
mattdp <matthew.frank.dupont@gmail.com>
2013-07-17 18:30:06 -0700

    users can't see other users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
 
   before_filter :signed_in_user, only: [:index, :edit, :update, :destroy]
-  before_filter :correct_user,   only: [:edit, :update]
+  before_filter :correct_user,   only: [:show, :edit, :update, :destroy]
   before_filter :admin_user,     only: [:destroy]
 
   # GET /users
