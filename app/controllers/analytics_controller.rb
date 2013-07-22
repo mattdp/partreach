9f02e657a782f568e25535e2c38a16d50877228e
mattdp <matthew.frank.dupont@gmail.com>
2013-07-22 14:04:29 -0700

    style and refactor to DRY

--- app/controllers/analytics_controller.rb
+++ app/controllers/analytics_controller.rb
@@ -9,8 +9,13 @@ class AnalyticsController < ApplicationController
 	end
 
 	def suppliers
-		@signed = [Supplier.first]
-		@claimed = [Supplier.last]
+		signed = Supplier.quantity_by_tag_id("all",Tag.find_by_name("n3_signedAndNDAd").id)
+		signed = signed.concat(Supplier.quantity_by_tag_id("all",Tag.find_by_name("n5_signed_only").id))
+		claimed = Supplier.where("claimed = true")
+		@listings = {
+			"Suppliers that are signed:" => signed,
+			"Suppliers that have claimed profiles:" => claimed
+		}		
 	end
 
 	def emails
