484b331172c43a3830f560c40acdd4c3cb8d54b0
mattdp <matthew.frank.dupont@gmail.com>
2013-08-19 11:48:54 -0700

    first piece of analysis

--- app/controllers/analytics_controller.rb
+++ app/controllers/analytics_controller.rb
@@ -21,4 +21,21 @@ class AnalyticsController < ApplicationController
 	def emails
 	end
 
+	def metrics
+		potential_date = Date.new(2013,3,2)
+		dates = []
+		while potential_date < Date.today
+			dates << potential_date
+			potential_date = potential_date + 7
+		end
+		printout = [["Week", "Leads"]] #titles
+		dates.each do |date|
+			unit = []
+			unit << date
+			unit << Lead.where("created_at > ? AND created_at < ?", date, date + 7).count
+			printout << unit
+		end
+		@printout = printout
+	end
+
 end
