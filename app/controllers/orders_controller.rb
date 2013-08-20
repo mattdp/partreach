09aacb29881bd0d5c70541f02ba0510dffd52e69
mattdp <matthew.frank.dupont@gmail.com>
2013-08-20 16:59:19 -0700

    copy and links for supplier index page

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -7,6 +7,7 @@ class OrdersController < ApplicationController
   # GET /orders.json
   def index
     @orders = current_user.orders
+    current_user.supplier_id.nil? ? @supplier = nil : @supplier = Supplier.find(current_user.supplier_id)
 
     respond_to do |format|
       format.html # index.html.erb
