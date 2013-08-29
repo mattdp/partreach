53423e1bdc136f3647560ed2116c32fb65209034
mattdp <matthew.frank.dupont@gmail.com>
2013-08-29 15:39:15 -0700

    set secret token

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -5,3 +5,4 @@
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
 Partreach::Application.config.secret_token = '888cf26c58c3b252b333d853910d07d8e3b695345b62cf1a4898ccc5d83cddcf834d2f96c1e04e41cec100bea1b4ebb759b319b4917c9a323344681039af82ad'
+Partreach::Application.config.secret_key_base = 'x888cf26c58c3b252b333d853910d07d8e3b695345b62cf1a4898ccc5d83cddcf834d2f96c1e04e41cec100bea1b4ebb759b319b4917c9a323344681039af82ad'
