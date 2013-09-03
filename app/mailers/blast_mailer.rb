e4795ff35fa9934a021ac12b6fd5b30053f7f3d5
mattdp <matthew.frank.dupont@gmail.com>
2013-09-03 15:42:50 -0700

    untested mailtos in email

--- app/mailers/blast_mailer.rb
+++ app/mailers/blast_mailer.rb
@@ -23,7 +23,7 @@ class BlastMailer < ActionMailer::Base
     @supplier = supplier
     @brand_name = brand_name
     @url_name_for_link = supplier_profile_path(supplier.name_for_link)
-    @url_id = edit_supplier_path(supplier.id)
+    @url_edit = edit_supplier_path(supplier.id)
     mail(to: supplier.email, subject:"Customers want to know more about #{supplier.name} on #{brand_name}!")
   end
 
