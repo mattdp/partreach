4c16ccce57782bfc1ab447cdbb65c5b3f668596f
mattdp <matthew.frank.dupont@gmail.com>
2013-09-04 11:57:10 -0700

    add password reset event

--- app/models/user.rb
+++ app/models/user.rb
@@ -56,6 +56,7 @@ class User < ActiveRecord::Base
     generate_token(:password_reset_token)
     self.password_reset_sent_at = Time.zone.now
     self.save!(:validate => false)
+    Event.add_event("User",self.id,"requested_password_reset")
     if supplier_id.nil?
       UserMailer.password_reset(self).deliver
     else
