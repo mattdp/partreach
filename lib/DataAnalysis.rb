b5bad37aa6fd2c781bf74e311d0acddbf36547de
mattdp <matthew.frank.dupont@gmail.com>
2013-09-04 18:34:20 -0700

    asks_data to analyze supplier asks in DataAnalysis

--- lib/DataAnalysis.rb
+++ lib/DataAnalysis.rb
@@ -1,5 +1,22 @@
 module DataAnalysis
 
+	def asks_data
+		answer = {}
+		all_real_asks = Ask.where("real = true")
+		Supplier.find_each do |s|
+			supplier_asks = all_real_asks.where("supplier_id = ?",s.id)
+			total = supplier_asks.count
+			if total > 0
+				answer[s.name] = [
+					supplier_asks.where("request = 'description_requested'").count,
+					supplier_asks.where("request = 'machines_requested'").count,
+					supplier_asks.where("request = 'reviews_requested'").count
+				]
+			end
+		end
+		return answer
+	end
+
 	def paid_supplier_quotes
 		op = "Order date\tOrder #\tSupplier_id\tSupplier_name\tTotal cost\n"
 		Dialogue.find_each do |d|
