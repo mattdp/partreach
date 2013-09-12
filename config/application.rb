44fe21d927cb84f312ae15c4bd6a667259d6d5e2
mattdp <matthew.frank.dupont@gmail.com>
2013-09-12 15:40:54 -0700

    attempting dailli compression

--- config/application.rb
+++ config/application.rb
@@ -7,7 +7,7 @@ Bundler.require(:default, Rails.env)
 module Partreach
   class Application < Rails::Application
 
-    config.cache_store = :dalli_store
+    config.cache_store = :dalli_store, {compress: true}
 
     #based on https://devcenter.heroku.com/articles/rails-asset-pipeline
     config.assets.initialize_on_precompile = false
