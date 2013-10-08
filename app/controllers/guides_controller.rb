f492af775903762d5f34dc11a93da91de35f5aa1
mattdp <matthew.frank.dupont@gmail.com>
2013-10-08 16:23:44 -0700

    initial geo and tag blurbs, handle no suppliers or unauthorized url

--- app/controllers/guides_controller.rb
+++ app/controllers/guides_controller.rb
@@ -1,10 +1,12 @@
 class GuidesController < ApplicationController
 
+	#only works for all of country, state, tag being present and nicely formatted
 	def show
 		@country = params[:country].upcase
 		@state = params[:state].upcase
 		@tag = Tag.find_by_name_for_link(params[:tag_name_for_link])
 		@suppliers = Supplier.quantity_by_tag_id("all",@tag.id,@country,@state)
+		@valid_guide = true
 	end
 
 end
