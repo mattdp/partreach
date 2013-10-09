6d4051f9b347b65dfca8b85209c8a1e173229a1d
mattdp <matthew.frank.dupont@gmail.com>
2013-10-08 17:00:13 -0700

    dia new sorting

--- app/assets/javascripts/dialogues.js
+++ app/assets/javascripts/dialogues.js
@@ -5,37 +5,37 @@ $(document).ready(function() {
 		$("td:not(.hidden)").find(".supplier-checkbox").attr("checked","true")
 	});
 
-});
+	//allow multitag searching
+	$(document).find('#tag_matrix').find('input').change(function() {
+		if($('#add-dialogues-radio').is(':checked')) {
+			$('#supplier_matrix').find('td').addClass('hidden');
 
-//allow multitag searching
-$(document).find('#tag_matrix').find('input').change(function() {
-	if($('#add-dialogues-radio').is(':checked')) {
-		$('#supplier_matrix').find('td').addClass('hidden');
-
-		//get a list of all checked boxes
-		var checkedBoxes = [];
-		$('#tag_matrix').find('input').each (function () {
-			if($(this).is(':checked')) {
-				checkedBoxes.push($(this).closest('td').find('p').text());
-			}
-		});
-
-		var activeCheckbox = true;
-		var unhide = true;
-		var i = 0;
-
-		//reveal all that have each criterion
-		$('#supplier_matrix').find('input').each (function () {
-			activeCheckbox = $(this);
-			unhide = true;
-			for (i = 0; i < checkedBoxes.length; i = i + 1) {
-				if (!activeCheckbox.hasClass(checkedBoxes[i])) {
-					unhide = false;
+			//get a list of all checked boxes
+			var checkedBoxes = [];
+			$('#tag_matrix').find('input').each (function () {
+				if($(this).is(':checked')) {
+					checkedBoxes.push($(this).closest('td').find('p').text());
 				}
-			}
-			if (unhide) {
-				activeCheckbox.closest('td').removeClass('hidden');
-			}
-		});
-	}
+			});
+
+			var activeCheckbox = true;
+			var unhide = true;
+			var i = 0;
+
+			//reveal all that have each criterion
+			$('#supplier_matrix').find('input').each (function () {
+				activeCheckbox = $(this);
+				unhide = true;
+				for (i = 0; i < checkedBoxes.length; i = i + 1) {
+					if (!activeCheckbox.hasClass(checkedBoxes[i])) {
+						unhide = false;
+					}
+				}
+				if (unhide) {
+					activeCheckbox.closest('td').removeClass('hidden');
+				}
+			});
+		}
+	});
+
 });
