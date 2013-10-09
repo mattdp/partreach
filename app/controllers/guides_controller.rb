0a51c18bb8638bf2e1171108495a065dc5f3a648
mattdp <matthew.frank.dupont@gmail.com>
2013-10-09 11:37:33 -0700

    only some valid guides

--- app/controllers/guides_controller.rb
+++ app/controllers/guides_controller.rb
@@ -4,11 +4,22 @@ class GuidesController < ApplicationController
 	def show
 		@country = Word.transform(:name_for_link,params[:country],:shortform)
 		@state = Word.transform(:name_for_link,params[:state],:shortform)
+		@state_long = Word.transform(:name_for_link,params[:state],:longform)
 		@tag = Tag.find_by_name_for_link(params[:tag_name_for_link])
-		supplier_holder = Supplier.visible_profiles_sorted(nil,@tag,@country,@state)
-		@supplier_information_arrays = []
-		@supplier_information_arrays = supplier_holder[@country][@state] if supplier_holder.present?
-		@valid_guide = true
+		@valid_guide = valid_guide?(@country,@state,@tag.name_for_link)
+		if @valid_guide
+			supplier_holder = Supplier.visible_profiles_sorted(nil,@tag,@country,@state)
+			@supplier_information_arrays = supplier_holder[@country][@state] if supplier_holder.present?
+		end
 	end
+
+	private
+
+		def valid_guide?(country,state,tag_name)
+			comparitor = [country,state,tag_name]
+			return comparitor.in?([
+				["US","CA","3dprinting"]
+			])
+		end
 
 end
