0d5d36ab4b2397f494c7d64177879125e24a89d6
mattdp <matthew.frank.dupont@gmail.com>
2013-10-14 09:55:50 -0700

    tweaks

--- app/controllers/guides_controller.rb
+++ app/controllers/guides_controller.rb
@@ -2,13 +2,15 @@ class GuidesController < ApplicationController
 
 	#only works for all of country, state, tag being present and nicely formatted
 	def show
-		@country = Word.transform(:name_for_link,params[:country],:shortform)
-		@country_long = Word.transform(:name_for_link,params[:country],:longform)
-		@state = Word.transform(:name_for_link,params[:state],:shortform)
-		@state_long = Word.transform(:name_for_link,params[:state],:longform)
-		@tag = Tag.find_by_name_for_link(params[:tag_name_for_link])
-		id_string = "#{@country}-#{@state}-#{@tag.name}"
-		@filter = Filter.get(id_string)
+		if params[:country] and params[:state] and params[:tag_name_for_link]
+			@country = Word.transform(:name_for_link,params[:country],:shortform)
+			@country_long = Word.transform(:name_for_link,params[:country],:longform)
+			@state = Word.transform(:name_for_link,params[:state],:shortform)
+			@state_long = Word.transform(:name_for_link,params[:state],:longform)
+			@tag = Tag.find_by_name_for_link(params[:tag_name_for_link])
+			id_string = "#{@country}-#{@state}-#{@tag.name}"
+			@filter = Filter.get(id_string)
+		end #put in elsif for index of country/tag OR make that a CST.
 		if @filter
 			@visibles, @supplier_count = Rails.cache.fetch id_string, :expires_in => 25.hours do |key|
 				logger.debug "Cache miss: #{id_string}"
