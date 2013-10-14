8a5b32e13ba4c936f467f6252921bf0439785602
mattdp <matthew.frank.dupont@gmail.com>
2013-10-14 11:12:40 -0700

    retooled guide code

--- app/controllers/guides_controller.rb
+++ app/controllers/guides_controller.rb
@@ -3,14 +3,22 @@ class GuidesController < ApplicationController
 	#only works for all of country, state, tag being present and nicely formatted
 	def show
 		if params[:country] and params[:state] and params[:tag_name_for_link]
-			@country = Word.transform(:name_for_link,params[:country],:shortform)
-			@country_long = Word.transform(:name_for_link,params[:country],:longform)
-			@state = Word.transform(:name_for_link,params[:state],:shortform)
-			@state_long = Word.transform(:name_for_link,params[:state],:longform)
-			@tag = Tag.find_by_name_for_link(params[:tag_name_for_link])
-			id_string = "#{@country}-#{@state}-#{@tag.name}"
+			country = Word.transform(:name_for_link,params[:country],:shortform)
+			state = Word.transform(:name_for_link,params[:state],:shortform)
+			tag = Tag.find_by_name_for_link(params[:tag_name_for_link])
+			country_long = Word.transform(:name_for_link,params[:country],:longform)
+			state_long = Word.transform(:name_for_link,params[:state],:longform)
+
+			id_string = "#{country}-#{state}-#{tag.name}"
 			@filter = Filter.get(id_string)
-		end #put in elsif for index of country/tag OR make that a CST.
+
+			state.present? ? @location_phrase = "#{state_long} (#{country_long})" : @location_phrase = "#{country_long}"
+			@tags_name = tag.readable
+			@tags_note = tag.note
+
+		elsif params[:index_name]
+			@filter = Filter.get(params[:index_name])
+		end
 		if @filter
 			@visibles, @supplier_count = Rails.cache.fetch id_string, :expires_in => 25.hours do |key|
 				logger.debug "Cache miss: #{id_string}"
