178eeddbb49d81e690395e644059b62988061da7
mattdp <matthew.frank.dupont@gmail.com>
2013-10-15 11:31:49 -0700

    attempt password update fix by strong paramifying

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -14,14 +14,21 @@ class PasswordResetsController < ApplicationController
 
   def update
   	@user = User.find_by_password_reset_token!(params[:id])
-  	if @user.password_reset_sent_at < 24.hours.ago
-  		redirect_to new_password_reset_path, :alert => "Password reset has expired. Try again."
-  	elsif @user.update_attributes(params[:user])
+    hours_allowed = 48
+  	if @user.password_reset_sent_at < hours_allowed.hours.ago
+  		redirect_to new_password_reset_path, :alert => "Password reset has expired (Each reset is good for #{hours_allowed} hours). Try again."
+  	elsif @user.update_attributes(password_reset_params)
       sign_in @user
   		redirect_to orders_path, :notice => "Password has been set. You are now logged in."
   	else
   		render :edit
   	end
   end
+
+  private
+
+    def password_reset_params
+      params.permit(:user)
+    end
 
 end
