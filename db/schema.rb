4022d455121776007e4b599c3d15fc92ca630bfc
mattdp <matthew.frank.dupont@gmail.com>
2013-11-12 13:06:36 -0800

    index for tag names to accelerate supplier profile picking for lps

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131111200421) do
+ActiveRecord::Schema.define(version: 20131112210308) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -231,6 +231,8 @@ ActiveRecord::Schema.define(version: 20131111200421) do
     t.string   "readable"
     t.string   "name_for_link"
   end
+
+  add_index "tags", ["name"], name: "index_tags_on_name", unique: true, using: :btree
 
   create_table "uploads", force: true do |t|
     t.string   "upload_file_name"
