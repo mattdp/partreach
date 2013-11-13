93c12e7ec0f17e1cb9470354c6ccf14950978910
mattdp <matthew.frank.dupont@gmail.com>
2013-11-13 13:22:19 -0800

    jquery for summary

---
+++ app/assets/javascripts/static_pages.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+	
+	var classList = 0;
+	var summary = "";
+
+	$('.question_radio_button').on('click', function(event) {
+		summary = $(this).data("summary");
+		classList = this.className.split(/\s+/);
+		$.each(classList, function(index, item){
+			if (item === 'experience') {
+				$('.summary_experience').text(summary);
+			}
+			else if (item === 'priority') {
+				$('.summary_priority').text(summary);
+			}
+			else if (item === 'manufacturing') {
+				$('.summary_manufacturing').text(summary);
+			}
+		});
+	});
+});
