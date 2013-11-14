74a9c4a52cc5946716d61cf7c37166cddcd1dcba
mattdp <matthew.frank.dupont@gmail.com>
2013-11-14 02:31:35 -0800

    finally resolved border color battle

--- app/assets/javascripts/profiles.js
+++ app/assets/javascripts/profiles.js
@@ -3,28 +3,28 @@ $(document).ready(function() {
 		event.preventDefault();
 		$('.supplier-variable-content').children().addClass('hidden');
 		$('.description-content').removeClass('hidden');
-		$('.options-menu').find('li').removeClass('active')
+		$('.nav-tabs').find('li').removeClass('active')
 		$('.description-toggle').closest('li').addClass('active');
 	});
 	$('.machines-toggle').on('click', function(event) {
 		event.preventDefault();
 		$('.supplier-variable-content').children().addClass('hidden');
 		$('.machines-content').removeClass('hidden');
-		$('.options-menu').find('li').removeClass('active')
+		$('.nav-tabs').find('li').removeClass('active')
 		$('.machines-toggle').closest('li').addClass('active');
 	});
 	$('.reviews-toggle').on('click', function(event) {
 		event.preventDefault();
 		$('.supplier-variable-content').children().addClass('hidden');
 		$('.reviews-content').removeClass('hidden');
-		$('.options-menu').find('li').removeClass('active')
+		$('.nav-tabs').find('li').removeClass('active')
 		$('.reviews-toggle').closest('li').addClass('active');
 	});
 	$('.photos-toggle').on('click', function(event) {
 		event.preventDefault();
 		$('.supplier-variable-content').children().addClass('hidden');
 		$('.photos-content').removeClass('hidden');
-		$('.options-menu').find('li').removeClass('active')
+		$('.nav-tabs').find('li').removeClass('active')
 		$('.photos-toggle').closest('li').addClass('active');
 	});
 });
