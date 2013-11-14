950c4e3b80019d3c5165166e01717dfc9ad73eaa
mattdp <matthew.frank.dupont@gmail.com>
2013-11-14 12:55:38 -0800

    alerts showing up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
 
-  before_filter :signed_in_user, only: [:index, :edit, :update, :destroy]
+  before_filter :signed_in_user
   before_filter :correct_user,   only: [:show, :edit, :update, :destroy]
   before_filter :admin_user,     only: [:destroy]
 
