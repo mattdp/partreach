d1938b6412e22c0c6d933ecf76e29003ad380ebd
mattdp <matthew.frank.dupont@gmail.com>
2013-11-15 12:42:12 -0800

    better error messaging

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,7 +9,7 @@ class SessionsController < ApplicationController
       sign_in user
       redirect_back_or orders_path
     else
-      flash.now[:error] = 'Invalid email/password combination'
+      flash.now[:danger] = 'Invalid email/password combination'
       render 'new'
     end
 	end
