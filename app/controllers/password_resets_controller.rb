387ae7b847418f85e9d9364a59b75f60dd0da2e7
mattdp <matthew.frank.dupont@gmail.com>
2013-11-17 14:27:36 -0800

    pw reset error catching

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -1,4 +1,7 @@
 class PasswordResetsController < ApplicationController
+  
+  HOURS_ALLOWED = 48
+
   def new
   end
 
@@ -9,13 +12,13 @@ class PasswordResetsController < ApplicationController
   end
 
   def edit
-  	@user = User.find_by_password_reset_token!(params[:id])
+  	@user = User.find_by_password_reset_token(params[:id])
+    @hours_allowed = HOURS_ALLOWED
   end
 
   def update
   	@user = User.find_by_password_reset_token!(params[:id])
-    hours_allowed = 48
-  	if @user.password_reset_sent_at < hours_allowed.hours.ago
+  	if @user.password_reset_sent_at < HOURS_ALLOWED.hours.ago
   		redirect_to new_password_reset_path, :alert => "Password reset has expired (Each reset is good for #{hours_allowed} hours). Try again."
   	elsif @user.update_attributes(password_reset_params)
       sign_in @user
