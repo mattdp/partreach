d38ae23bb72533d1f97cbe492b7f8dbe12bb2c59
mattdp <matthew.frank.dupont@gmail.com>
2013-11-18 14:47:40 -0800

    test order for question checkboxes and supplier name carrying over from profile

--- spec/features/order_creation_spec.rb
+++ spec/features/order_creation_spec.rb
@@ -3,6 +3,7 @@ require 'spec_helper'
 feature "Order creation" do
 	background do
 		@existing_user = FactoryGirl.create(:user)
+		@visible_supplier = FactoryGirl.create(:supplier)
 	end
 
 	scenario "Creating order without fillout shouldn't make an order" do
@@ -27,12 +28,26 @@ feature "Order creation" do
 		#PICK UP AND DEBUG HERE
 
 		#order should be created
+		save_and_open_page
 		page.should have_content "Details of quote request"
 
 		#user should be saved
 		#they should be taken to appropriate page
 		#we should get an email about it
 
+	end
+
+	scenario "Clicking create off supplier page and selecting options should lead to correct options on final page" do
+		visit supplier_profile_path(@visible_supplier.name_for_link)
+		page.should have_content "Request Quote"
+		page.click_on "Request Quote"
+
+		page.should have_content "Help us ask you the right questions"
+		find(:css,"#questions_experience_experienced").set(true)
+		page.click_button "Final step"
+
+		page.should have_content "I am experienced"
+		page.should have_content "Include #{@visible_supplier.name}"
 	end
 
 end
