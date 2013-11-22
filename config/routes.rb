ba255750e8a82514a148cd55f37015d8811fad68
mattdp <matthew.frank.dupont@gmail.com>
2013-11-22 12:06:54 -0800

    machine delete seems to work

--- config/routes.rb
+++ config/routes.rb
@@ -35,7 +35,7 @@ Partreach::Application.routes.draw do
   get '/guides/:country/:state/:tags_string', to: 'guides#show', as: 'guide_cst'
   get '/guides/:name', to: 'guides#show', as: 'guide_name'
   get '/owners/new/:supplier_id', to: 'owners#new', as: 'new_owner'
-  match '/owners/destroy/:supplier_id/:machine_id', to: 'owners#destroy', via: :delete
+  match '/owners/destroy/:supplier_id/:machine_id', to: 'owners#destroy', as: 'destroy_owner', via: :delete
   get '/questions', to: 'static_pages#questions', as: 'order_questions'
 
   get '/analytics', to: 'analytics#home', as: 'analytics_home'
