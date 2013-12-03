486a27a98008c20d549f28bf367ed35de6a1abcf
mattdp <matthew.frank.dupont@gmail.com>
2013-12-02 16:47:45 -0800

    post-rake

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131202153430) do
+ActiveRecord::Schema.define(version: 20131203004451) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,8 @@ ActiveRecord::Schema.define(version: 20131202153430) do
     t.string   "place_type"
     t.string   "country"
     t.text     "notes"
+    t.integer  "country_id"
+    t.integer  "state_id"
   end
 
   create_table "asks", force: true do |t|
