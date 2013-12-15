f2cc6e71420c9c95891680a72df777610b408281
mattdp <matthew.frank.dupont@gmail.com>
2013-12-15 13:01:39 -0800

    methods for horizontal linking adjacencies

--- app/models/filter.rb
+++ app/models/filter.rb
@@ -45,4 +45,26 @@ class Filter < ActiveRecord::Base
 		return phrase
 	end
 
+  #return filter with same tag in next-highest geo, or empty array if that doesn't exist
+  def same_tag_bigger_geo
+    above = self.geography.geography
+    return [] if above.nil?
+    match = Filter.where("geography_id = ? and has_tag_id = ?",above.id,self.has_tag_id)
+    return match if match
+    return []
+  end
+
+  #return array, possibly empty, of same-geo different-tag filters
+  def same_geo_adjacencies(quantity_max = 5)
+    possibles = Filter.where("geography_id = ?",self.geography.id) #will always return at least self
+    possibles = possibles.delete_if{|filter| filter.id == self.id}
+    return possibles.take(quantity_max)
+  end
+
+  #array of 0-n filters to display for horizontal linking
+  def adjacencies
+    same_geo_adjacencies_quantity_max = 5
+    return self.same_geo_adjacencies(same_geo_adjacencies_quantity_max).concat(self.same_tag_bigger_geo)
+  end
+
 end
