10e061c9175ebc7c41ff410e2f300fd05b049d64
mattdp <matthew.frank.dupont@gmail.com>
2014-01-04 15:14:39 -0800

    tested lead update

--- app/controllers/leads_controller.rb
+++ app/controllers/leads_controller.rb
@@ -23,6 +23,17 @@ class LeadsController < ApplicationController
 	end
 
 	def update
+		@lead = Lead.find(params[:id])
+		@lead.update_attributes(lead_params)
+
+		redirect_to edit_lead_path(@lead), notice: "Lead update attempted."
 	end
+
+	private
+
+		def lead_params
+			params.require(:lead).permit(	:first_name,:last_name,:source,:company,:title,:notes,\
+																		:next_contact_date, :next_contact_content)
+		end
 
 end
