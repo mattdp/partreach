2399af941ba6c77d32f5e2551e0a1438ef9266d7
mattdp <matthew.frank.dupont@gmail.com>
2014-01-21 17:44:15 -0800

    organize routes by controller

--- config/routes.rb
+++ config/routes.rb
@@ -15,34 +15,6 @@ Partreach::Application.routes.draw do
   resources :machines, only: [:new, :create, :edit, :update]
   resources :owners, only: [:create]
 
-  get '/signup', to: 'static_pages#questions'
-  get '/signin', to: 'sessions#new', as: 'signin'
-  match '/signout', to: 'sessions#destroy', via: :delete
-  get '/getting_started', to: 'static_pages#getting_started'
-  get '/procurement', to: 'static_pages#procurement'
-  get '/materials', to: 'static_pages#materials'
-  get '/be_a_supplier', to: 'static_pages#be_a_supplier'
-  get '/manipulate/:id', to: 'orders#manipulate_dialogues', as: 'manipulate'
-  match '/orders/update_dialogues', to: 'orders#update_dialogues', via: :post
-  get '/orders/purchase/:order/:dialogue', to: 'orders#purchase'
-  get '/dialogues/new/:id', to: 'dialogues#new'
-  get '/dialogues/initial_email/:id', to: 'dialogues#initial_email', as: 'dialogue_internal_email'  
-  get '/profiles/:name', to: 'profiles#supplier_profile', as: 'supplier_profile'
-  get 'suppliers/admin_edit/:name', to: 'suppliers#admin_edit', as: 'admin_edit'
-  match 'suppliers/admin_update', to: 'suppliers#admin_update', as: 'admin_update', via: :post
-  get '/submit_ask/', to: 'profiles#submit_ask'
-  get '/terms', to: 'static_pages#terms'
-  get '/privacy', to: 'static_pages#privacy'
-  get '/guides/:name', to: 'guides#show', as: 'guide_name'
-  get '/owners/new/:supplier_id', to: 'owners#new', as: 'new_owner'
-  match '/owners/destroy/:supplier_id/:machine_id', to: 'owners#destroy', as: 'destroy_owner', via: :delete
-  get '/questions', to: 'static_pages#questions', as: 'order_questions'
-  get '/machines/suppliers_with_machine/:machine_id', to: 'machines#suppliers_with_machine', as: 'suppliers_with_machine'
-  get '/recruiting', to: 'static_pages#recruiting', as: 'recruiting'
-  match '/leads/admin_create', to: 'leads#admin_create', as: 'admin_create_lead', via: :post
-  get '/manufacturers/:manufacturer_name/:machine_name', to: 'machines#show', as: 'show_machine'
-  get '/manufacturers/:manufacturer_name', to: 'manufacturers#show', as: 'manufacturer'
-
   get '/analytics', to: 'analytics#home', as: 'analytics_home'
   get '/analytics/rfqs', to: 'analytics#rfqs', as: 'analytics_rfqs'
   get '/analytics/suppliers', to: 'analytics#suppliers', as: 'analytics_suppliers'
@@ -51,7 +23,45 @@ Partreach::Application.routes.draw do
   get '/analytics/machines', to: 'analytics#machines', as: 'analytics_machines'
   get '/analytics/phone', to: 'analytics#phone', as: 'analytics_phone'
 
+  get '/dialogues/new/:id', to: 'dialogues#new'
+  get '/dialogues/initial_email/:id', to: 'dialogues#initial_email', as: 'dialogue_internal_email'  
+
   get '/examinations/:name', to: 'examinations#setup_examinations', as: "setup_examinations"
   match '/examinations', to: 'examinations#submit_examinations', via: :post, as: "submit_examinations"
+
+  get '/guides/:name', to: 'guides#show', as: 'guide_name'
+
+  match '/leads/admin_create', to: 'leads#admin_create', as: 'admin_create_lead', via: :post
+
+  get '/manufacturers/:manufacturer_name/:machine_name', to: 'machines#show', as: 'show_machine'
+  get '/machines/suppliers_with_machine/:machine_id', to: 'machines#suppliers_with_machine', as: 'suppliers_with_machine'
+
+  get '/manufacturers/:manufacturer_name', to: 'manufacturers#show', as: 'manufacturer'
+
+  get '/profiles/:name', to: 'profiles#supplier_profile', as: 'supplier_profile'
+  get '/submit_ask/', to: 'profiles#submit_ask'
+  
+  get '/manipulate/:id', to: 'orders#manipulate_dialogues', as: 'manipulate'
+  match '/orders/update_dialogues', to: 'orders#update_dialogues', via: :post
+  get '/orders/purchase/:order/:dialogue', to: 'orders#purchase'
+
+  get '/owners/new/:supplier_id', to: 'owners#new', as: 'new_owner'
+  match '/owners/destroy/:supplier_id/:machine_id', to: 'owners#destroy', as: 'destroy_owner', via: :delete
+
+  get '/signin', to: 'sessions#new', as: 'signin'
+  match '/signout', to: 'sessions#destroy', via: :delete
+
+  get '/signup', to: 'static_pages#questions' 
+  get '/getting_started', to: 'static_pages#getting_started'
+  get '/procurement', to: 'static_pages#procurement'
+  get '/materials', to: 'static_pages#materials'
+  get '/be_a_supplier', to: 'static_pages#be_a_supplier'
+  get '/terms', to: 'static_pages#terms'
+  get '/privacy', to: 'static_pages#privacy'  
+  get '/questions', to: 'static_pages#questions', as: 'order_questions'
+  get '/recruiting', to: 'static_pages#recruiting', as: 'recruiting'
+
+  get 'suppliers/admin_edit/:name', to: 'suppliers#admin_edit', as: 'admin_edit'
+  match 'suppliers/admin_update', to: 'suppliers#admin_update', as: 'admin_update', via: :post
 
 end
