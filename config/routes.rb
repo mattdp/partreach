da03925780fa6edb12314cb8249d5a4427c0c9e0
mattdp <matthew.frank.dupont@gmail.com>
2014-01-23 09:03:12 -0800

    alphabetize resources in routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,19 +2,6 @@ Partreach::Application.routes.draw do
 
   root :to => 'static_pages#home', via: :get
 
-  resources :signed_urls, only: :index
-  resources :orders, only: [:index, :show, :new, :create, :destroy]
-  resources :dialogues, only: [:new, :create]
-  resources :users, only: [:edit, :update, :show] # no index, no destroy 
-  resources :sessions, only: [:new, :create, :destroy]
-  resources :leads, only: [:new, :create, :edit, :index, :update]
-  resources :reviews, only: [:new, :create]
-  resources :password_resets
-  resources :suppliers, only: [:new, :create, :edit, :update, :index]
-  resources :communications, only: [:new, :create]
-  resources :machines, only: [:new, :create, :edit, :update]
-  resources :owners, only: [:create]
-
   get '/analytics', to: 'analytics#home', as: 'analytics_home'
   get '/analytics/rfqs', to: 'analytics#rfqs', as: 'analytics_rfqs'
   get '/analytics/suppliers', to: 'analytics#suppliers', as: 'analytics_suppliers'
@@ -23,6 +10,9 @@ Partreach::Application.routes.draw do
   get '/analytics/machines', to: 'analytics#machines', as: 'analytics_machines'
   get '/analytics/phone', to: 'analytics#phone', as: 'analytics_phone'
 
+  resources :communications, only: [:new, :create]
+
+  resources :dialogues, only: [:new, :create]
   get '/dialogues/new/:id', to: 'dialogues#new'
   get '/dialogues/initial_email/:id', to: 'dialogues#initial_email', as: 'dialogue_internal_email'  
 
@@ -31,8 +21,10 @@ Partreach::Application.routes.draw do
 
   get '/guides/:name', to: 'guides#show', as: 'guide_name'
 
+  resources :leads, only: [:new, :create, :edit, :index, :update]
   match '/leads/admin_create', to: 'leads#admin_create', as: 'admin_create_lead', via: :post
 
+  resources :machines, only: [:new, :create, :edit, :update]
   get '/machines/suppliers_with_machine/:machine_id', to: 'machines#suppliers_with_machine', as: 'suppliers_with_machine'
 
   get '/manufacturers/:manufacturer_name', to: 'profiles#manufacturer_profile', as: 'manufacturer_profile'
@@ -40,15 +32,24 @@ Partreach::Application.routes.draw do
   get '/profiles/:name', to: 'profiles#supplier_profile', as: 'supplier_profile'
   get '/submit_ask/', to: 'profiles#submit_ask'
   
+  resources :orders, only: [:index, :show, :new, :create, :destroy]
   get '/manipulate/:id', to: 'orders#manipulate_dialogues', as: 'manipulate'
   match '/orders/update_dialogues', to: 'orders#update_dialogues', via: :post
   get '/orders/purchase/:order/:dialogue', to: 'orders#purchase'
 
+  resources :owners, only: [:create]
   get '/owners/new/:supplier_id', to: 'owners#new', as: 'new_owner'
   match '/owners/destroy/:supplier_id/:machine_id', to: 'owners#destroy', as: 'destroy_owner', via: :delete
 
+  resources :password_resets
+
+  resources :reviews, only: [:new, :create]
+
+  resources :sessions, only: [:new, :create, :destroy]
   get '/signin', to: 'sessions#new', as: 'signin'
   match '/signout', to: 'sessions#destroy', via: :delete
+
+  resources :signed_urls, only: :index
 
   get '/signup', to: 'static_pages#questions' 
   get '/getting_started', to: 'static_pages#getting_started'
@@ -60,7 +61,10 @@ Partreach::Application.routes.draw do
   get '/questions', to: 'static_pages#questions', as: 'order_questions'
   get '/recruiting', to: 'static_pages#recruiting', as: 'recruiting'
 
+  resources :suppliers, only: [:new, :create, :edit, :update, :index]
   get 'suppliers/admin_edit/:name', to: 'suppliers#admin_edit', as: 'admin_edit'
   match 'suppliers/admin_update', to: 'suppliers#admin_update', as: 'admin_update', via: :post
+
+  resources :users, only: [:edit, :update, :show] # no index, no destroy 
 
 end
