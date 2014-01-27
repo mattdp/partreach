458226cf01d1dd93cc5b31993060b6836a415c5b
mattdp <matthew.frank.dupont@gmail.com>
2014-01-26 16:52:26 -0800

    new order group creation

--- app/controllers/order_groups_controller.rb
+++ app/controllers/order_groups_controller.rb
@@ -2,9 +2,20 @@ class OrderGroupsController < ApplicationController
 	before_filter :admin_user
 
 	def new
+		@order_group = OrderGroup.new
 	end
 
 	def create
+		@order_group = OrderGroup.create(order_group_params)
+		@order = @order_group.order
+
+		redirect_to manipulate_path(@order.id), notice: 'Order group create attempted.'
 	end
+
+	private
+
+		def order_group_params
+			params.require(:order_group).permit(:name,:process,:order_id)
+		end
 
 end
