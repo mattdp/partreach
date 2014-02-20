87517f4a7c227e3bed4017c553c979dddf4793d2
mattdp <matthew.frank.dupont@gmail.com>
2014-02-20 10:32:14 -0800

    attempt encoding note line

--- config/environment.rb
+++ config/environment.rb
@@ -1,6 +1,7 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
+#http://stackoverflow.com/questions/4188677/ruby-on-rails-3-incompatible-character-encodings-utf-8-and-ascii-8bit-with-i18
 Encoding.default_external = Encoding::UTF_8
 Encoding.default_internal = Encoding::UTF_8
 
