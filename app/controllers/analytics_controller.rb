aeb38c352851415fdd33c73d16065998201fca97
mattdp <matthew.frank.dupont@gmail.com>
2014-02-26 17:17:08 -0800

    pull metrics code into order model

--- app/controllers/analytics_controller.rb
+++ app/controllers/analytics_controller.rb
@@ -33,41 +33,11 @@ class AnalyticsController < ApplicationController
 	end
 
 	def metrics
-		interval = :months
-		tracking_start_date = Date.new(2013,8,3)
-		dates = []
 
-		if interval == :months
-			#list of months, including one to two beyond the current one
-			dates = ((tracking_start_date)..(Date.today+31)).map{|d| Date.new(d.year, d.month, 1) }.uniq
-		elsif interval == :weeks
-			while tracking_start_date < (Date.today + 7)
-				dates << tracking_start_date
-				tracking_start_date += 7
-			end
-		end
+		metrics = Order.metrics(:months,Date.new(2013,8,3))
+		@titles = metrics[:titles] 
+		@printout = metrics[:printout]
 
-		@titles = [interval.to_s,"Quote value of orders", "RFQ Creates", "Buyers touched by RFQs", "Suppliers touched by RFQs", "Closed RFQs","Reviews","Profiles claimed", "Suppliers joined network", "Leads and Users"]
-		printout = [] #titles
-		index = 0
-		#-2 since using dates[index] and dates[index+1]
-		while index <= dates.length - 2		
-			unit = []
-			created_orders = Order.where("created_at > ? AND created_at < ?", dates[index], dates[index+1])
-			unit << dates[index]
-			unit << created_orders.sum{|o| o.quote_value}.to_s
-			unit << created_orders.count
-			unit << created_orders.map{|o| o.user_id}.uniq.count
-			unit << created_orders.map{|o| o.dialogues}.flatten.select{|d| d.opener_sent}.map{|d| d.supplier_id}.uniq.count
-			unit << Event.where("created_at > ? AND created_at < ? AND model = ? AND happening = ?", dates[index], dates[index+1], "Order", "closed_successfully").count
-			unit << Review.where("created_at > ? AND created_at < ?", dates[index], dates[index+1]).count
-			unit << Event.where("created_at > ? AND created_at < ? AND model = ? AND happening = ?", dates[index], dates[index+1], "Supplier", "claimed_profile").count
-			unit << Event.where("created_at > ? AND created_at < ? AND model = ? AND happening = ?", dates[index], dates[index+1], "Supplier", "joined_network").count
-			unit << LeadContact.where("created_at > ? AND created_at < ?", dates[index], dates[index+1]).count
-			printout << unit
-			index += 1
-		end
-		@printout = printout.reverse
 	end
 
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -270,4 +270,44 @@ def add_complex_order(location)
     return snippet
   end
 
+  def self.metrics(interval,tracking_start_date)
+    output = {}
+    dates = []
+
+    if interval == :months
+      #list of months, including one to two beyond the current one
+      dates = ((tracking_start_date)..(Date.today+31)).map{|d| Date.new(d.year, d.month, 1) }.uniq
+    elsif interval == :weeks
+      while tracking_start_date < (Date.today + 7)
+        dates << tracking_start_date
+        tracking_start_date += 7
+      end
+    end
+
+    output[:titles] = [interval.to_s,"Quote value of orders", "RFQ Creates", "Buyers touched by RFQs", "Suppliers touched by RFQs", "Closed RFQs","Reviews","Profiles claimed", "Suppliers joined network", "Leads and Users"]
+
+    printout = []
+    index = 0
+    #-2 since using dates[index] and dates[index+1]
+    while index <= dates.length - 2   
+      unit = []
+      created_orders = Order.where("created_at > ? AND created_at < ?", dates[index], dates[index+1])
+      unit << dates[index]
+      unit << created_orders.sum{|o| o.quote_value}.to_s
+      unit << created_orders.count
+      unit << created_orders.map{|o| o.user_id}.uniq.count
+      unit << created_orders.map{|o| o.dialogues}.flatten.select{|d| d.opener_sent}.map{|d| d.supplier_id}.uniq.count
+      unit << Event.where("created_at > ? AND created_at < ? AND model = ? AND happening = ?", dates[index], dates[index+1], "Order", "closed_successfully").count
+      unit << Review.where("created_at > ? AND created_at < ?", dates[index], dates[index+1]).count
+      unit << Event.where("created_at > ? AND created_at < ? AND model = ? AND happening = ?", dates[index], dates[index+1], "Supplier", "claimed_profile").count
+      unit << Event.where("created_at > ? AND created_at < ? AND model = ? AND happening = ?", dates[index], dates[index+1], "Supplier", "joined_network").count
+      unit << LeadContact.where("created_at > ? AND created_at < ?", dates[index], dates[index+1]).count
+      printout << unit
+      index += 1
+    end
+    output[:printout] = printout.reverse
+
+    return output
+  end
+
 end
