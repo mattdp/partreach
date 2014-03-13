9bb395cc9bd580042d898e85b13e29dbab5b78df
James Bond <james.bond.home@gmail.com>
2014-03-13 15:15:27 -0700

    multi-file upload working (not much UI)

--- app/controllers/parts_controller.rb
+++ app/controllers/parts_controller.rb
@@ -2,8 +2,10 @@ class PartsController < ApplicationController
 
   def create_with_external
     p params
-    part = Part.create({order_group_id: params[:order_group_id], name: params[:filename]})
-    external = External.create({url: params[:file], \
+    puts params['filename']
+    puts "creating part with #{params['order_group_id']}, #{params['filename']}"
+    part = Part.create({order_group_id: params['order_group_id'], name: params['filename']})
+    external = External.create({url: params['url'], \
         consumer_id: part.id, consumer_type: "Part" }) if part
     render json: true
   end
