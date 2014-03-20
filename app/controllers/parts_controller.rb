99033017b45f759ebf16162d098154fc40f68c5e
James Bond <james.bond.home@gmail.com>
2014-03-20 15:56:42 -0700

    remove some debug code

--- app/controllers/parts_controller.rb
+++ app/controllers/parts_controller.rb
@@ -1,9 +1,7 @@
 class PartsController < ApplicationController
 
   def create_with_external
-    p params
-    puts params['filename']
-    puts "creating part with #{params['order_group_id']}, #{params['filename']}"
+    # puts "creating part with #{params['order_group_id']}, #{params['filename']}"
     part = Part.create({order_group_id: params['order_group_id'], name: params['filename']})
     external = External.create({url: params['url'], \
         consumer_id: part.id, consumer_type: "Part" }) if part
