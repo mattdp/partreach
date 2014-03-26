072d075eaac3383b04a39003d85e6e0e8bf7347c
James Bond <james.bond.home@gmail.com>
2014-03-26 12:05:57 -0700

    configure ActionMailer for different environments (so live emails aren't actually sent from dev/test env)

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -32,6 +32,9 @@ Partreach::Application.configure do
   # The :test delivery method accumulates sent emails in the
   # ActionMailer::Base.deliveries array.
   config.action_mailer.delivery_method = :test
+  # Don't care if the mailer can't send
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default_url_options = { :host => "localhost:3000" }
 
   # Print deprecation notices to the stderr
   config.active_support.deprecation = :stderr
