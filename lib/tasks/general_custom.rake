b0571bdbd37ea038b34b6c9069cbff40293c8940
James Bond <james.bond.home@gmail.com>
2014-04-03 08:24:56 -0700

    Google CSE search - add snippet; allow settomg start position and number of results

--- lib/tasks/general_custom.rake
+++ lib/tasks/general_custom.rake
@@ -169,13 +169,16 @@ task :search_google => :environment do
 	query = ENV["query"]
 	file = ENV["file"]
 	opts = {}
-	opts[:num] = ENV["num"]
-	opts[:start] = ENV["start"]
+	opts[:num] = ENV["num"] if ENV["num"]
+	opts[:start] = ENV["start"] if ENV["start"]
 
 	if (query)
 		WebSearch.search_google(query, file, opts)
 	else
-		puts 'Usage: rake search_google query="search terms" file=path/to/output_file.csv'
-		# puts 'Usage: rake search_google query="search terms" file=path/to/output_file.csv num=max_number_of_results start=starting_with'
+		puts 'Usage: rake search_google query="search terms" [OPTIONS]'
+		puts 'OPTIONS:'
+		puts '  file=path/to/output_file.csv'
+		puts '  num=max number of results'
+		puts '  start=starting position in results'
 	end
 end
--- lib/web_search.rb
+++ lib/web_search.rb
@@ -1,24 +1,30 @@
 class WebSearch
-	require 'csv'
+  require 'csv'
 
-	def self.search_google(query, output_file=nil, opts = {})
-		output_file ||= "#{query}.csv"
- 	  opts[:start] ||= 1
+  def self.search_google(query, output_file=nil, opts = {})
+    output_file ||= "#{query}.csv"
+    position = (opts[:start] ? opts[:start].to_i : 1)
+    num = opts[:num].to_i
+    results_count = 0
 
-		CSV.open(output_file, "wb") do |csv|
-		  csv << ["query", "position", "title", "url", "snippet"]
-		  position = 0
-	    begin
-	      page = GoogleCustomSearchApi.search(query,opts)
-	      break if page["error"]
-				print "."
-				page["items"].each do |item|
-				  position += 1
-				  csv << [query, position, item["title"], item["link"], item["snippet"]]
-				end
-	    end while opts[:start] = page.queries.nextPage.first.startIndex
+    CSV.open(output_file, "wb") do |csv|
+      csv << ["query", "position", "title", "url", "snippet"]
 
-		end
-	end
+      loop do
+        page = GoogleCustomSearchApi.search(query,opts)
+        return if page["error"]
+
+        print "." # show progress
+        page["items"].each do |item|
+          csv << [query, position, item["title"], item["link"], item["snippet"]]
+          position += 1
+          results_count += 1
+          return if opts[:num] && (results_count >= num)
+        end
+
+        opts[:start] = page.queries.nextPage.first.startIndex
+      end
+    end
+  end
 
 end
