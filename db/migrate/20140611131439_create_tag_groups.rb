126330f196155a84345b629cb6c99008ffd00e80
James Bond <james.bond.home@gmail.com>
2014-06-24 17:01:39 -0700

    do data migration as part of schema migration

--- db/migrate/20140611131439_create_tag_groups.rb
+++ db/migrate/20140611131439_create_tag_groups.rb
@@ -18,17 +18,31 @@ class CreateTagGroups < ActiveRecord::Migration
     add_index :taggables, [:type_name, :tag_group_id], unique: true
 
 
-    puts <<-EOS
-*
-*
-********************************************************************************************
-*
-* AFTER PERFORMING MIGRATION, BE SURE TO RUN db/scripts/migrate_tag_family_to_tag_groups.sql
-*
-********************************************************************************************
-*
-*
-    EOS
+    reversible do |dir|
+      # one-time data migration
+      dir.up do
+        # create tag_groups from existing tags
+        sql = <<-SQL
+          INSERT INTO tag_groups (group_name, exclusive, created_at, updated_at)
+          SELECT DISTINCT family, exclusive, now(), now() FROM tags;
+        SQL
+        TagGroup.connection.execute(sql)
 
+        # set FK in tags
+        sql = <<-SQL
+          UPDATE tags
+          SET tag_group_id = tag_groups.id, updated_at = now()
+          FROM tag_groups WHERE group_name = family;
+        SQL
+        TagGroup.connection.execute(sql)
+
+        # make all of these tag groups applicable to Suppliers
+        sql = <<-SQL
+          INSERT INTO taggables (type_name, tag_group_id, created_at, updated_at)
+          SELECT 'Supplier', id, now(), now() FROM tag_groups;
+        SQL
+        Taggable.connection.execute(sql)
+      end
+    end
   end
 end
