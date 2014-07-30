8ffd632911593748f67468a8afe143573132661f
James Bond <james.bond.home@gmail.com>
2014-07-30 09:29:52 -0700

    recover previous dialogues.js code, inadvertently overwritten

--- app/assets/javascripts/dialogues.js
+++ app/assets/javascripts/dialogues.js
@@ -1,4 +1,44 @@
 $(document).ready(function() {
+
+  //select all visible suppliers
+  $('#all-suppliers').click(function() {
+    $("td:not(.hidden)").find(".supplier-checkbox").attr("checked","true")
+  });
+
+  //allow multitag searching
+  $(document).find('.tag_matrix').find('input').change(function() {
+    if($('#add-dialogues-radio').is(':checked')) {
+      $('#supplier_matrix').find('td').addClass('hidden');
+
+      //get a list of all checked boxes
+      var checkedBoxes = [];
+      $('.tag_matrix').find('input').each (function () {
+        if($(this).is(':checked')) {
+          checkedBoxes.push($(this).attr('id'));
+        }
+      });
+
+      var activeCheckbox = true;
+      var unhide = true;
+      var i = 0;
+
+      //reveal all that have each criterion
+      $('#supplier_matrix').find('input').each (function () {
+        activeCheckbox = $(this);
+        unhide = true;
+        for (i = 0; i < checkedBoxes.length; i = i + 1) {
+          if (!activeCheckbox.hasClass(checkedBoxes[i])) {
+            unhide = false;
+          }
+        }
+        if (unhide) {
+          activeCheckbox.closest('td').removeClass('hidden');
+        }
+      });
+    }
+  });
+
+  // get rfq close email template
   $('#email_type').change(function() {
     id = $('#id').val();
     email_type = $('#email_type option:selected').val();
@@ -18,6 +58,5 @@ $(document).ready(function() {
       });
     }
   });
+
 });
-
-
