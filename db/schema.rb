a9f2788e492237b1843524dc518acc4f9e3bcdac
James Bond <james.bond.home@gmail.com>
2014-09-03 17:18:33 -0700

    updated schema from db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -312,15 +312,28 @@ ActiveRecord::Schema.define(version: 20140903165133) do
 
   add_index "tag_groups", ["group_name"], name: "index_tag_groups_on_group_name", unique: true, using: :btree
 
-  create_table "tag_relationships", force: true do |t|
-    t.integer  "source_tag_id",  null: false
-    t.integer  "related_tag_id", null: false
+  create_table "tag_relationship_types", force: true do |t|
+    t.string   "name"
+    t.integer  "source_group_id"
+    t.integer  "related_group_id"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "relationship"
   end
 
+  add_index "tag_relationship_types", ["related_group_id"], name: "index_tag_relationship_types_on_related_group_id", using: :btree
+  add_index "tag_relationship_types", ["source_group_id"], name: "index_tag_relationship_types_on_source_group_id", using: :btree
+
+  create_table "tag_relationships", force: true do |t|
+    t.integer  "source_tag_id",            null: false
+    t.integer  "related_tag_id",           null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.integer  "tag_relationship_type_id", null: false
+  end
+
+  add_index "tag_relationships", ["related_tag_id"], name: "index_tag_relationships_on_related_tag_id", using: :btree
   add_index "tag_relationships", ["source_tag_id"], name: "index_tag_relationships_on_source_tag_id", using: :btree
+  add_index "tag_relationships", ["tag_relationship_type_id", "source_tag_id", "related_tag_id"], name: "index_tag_relationships_unique", unique: true, using: :btree
 
   create_table "taggable_types", force: true do |t|
     t.string   "type_name",    null: false
