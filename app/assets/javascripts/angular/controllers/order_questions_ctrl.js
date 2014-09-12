b6555e0427a4c92c53dc9b2a0ef500d10b3c363b
Steven Harlow <stharlow@gmail.com>
2014-09-11 21:45:02 -0700

    summary is holdover from intermediate page

--- app/assets/javascripts/angular/controllers/order_questions_ctrl.js
+++ app/assets/javascripts/angular/controllers/order_questions_ctrl.js
@@ -17,7 +17,6 @@ App.controller('OrderQuestionsCtrl', ['$scope', 'UrlParams', function($scope, Ur
                 },
                 rookie: {
                     value: "rookie",
-                    summary: "am still learning",
                     punchy: "Still learning",
                     detail: "I'm not sure what's best for the job. I could use a little help."
                 }
@@ -28,19 +27,16 @@ App.controller('OrderQuestionsCtrl', ['$scope', 'UrlParams', function($scope, Ur
             values: {
                 speed: {
                   value: "speed",
-                  summary: "speed",
                   punchy: "Speed",
                   detail: "This needs to be done soon."
                 },
                 quality: {
                   value: "quality",
-                  summary: "quality",
                   punchy: "Quality",
                   detail: "The details have to be exactly right."
                 },
                 cost:{
                   value: "cost",
-                  summary: "cost",
                   punchy: "Cost",
                   detail: "Find me an inexpensive supplier."
                 }
@@ -51,19 +47,16 @@ App.controller('OrderQuestionsCtrl', ['$scope', 'UrlParams', function($scope, Ur
             values: {
                 printing: {
                   value: "printing",
-                  summary: "should be",
                   punchy: "3D printing",
                   detail: "I think this should be 3D printed."
                 },
                 other: {
                   value: "other",
-                  summary: "shouldn't be",
                   punchy: "Some other method",
                   detail: "3D printing isn't appropriate for this."
                 },
                 unknown: {
                   value: "unknown",
-                  summary: "might be",
                   punchy: "I don't know",
                   detail: "I don't know what's best."
                 }
