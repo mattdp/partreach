82dfc8a276047e43eaf9f34a49d4028284f1d507
James Bond <james.bond.home@gmail.com>
2014-09-19 20:58:10 -0700

    need to explicitly set nullable to true for reverse migration

--- db/migrate/20140915014355_add_unique_index_on_geographies_name_for_link.rb
+++ db/migrate/20140915014355_add_unique_index_on_geographies_name_for_link.rb
@@ -4,7 +4,7 @@ class AddUniqueIndexOnGeographiesNameForLink < ActiveRecord::Migration
     add_index :geographies, :name_for_link, :unique => true
   end
   def down
-    change_column :geographies, :name_for_link, :string
+    change_column :geographies, :name_for_link, :string, null: true
     remove_index :geographies, :name_for_link
   end
 end
