78ffe0f2fe5b60f9088a2dcd4372212e071cf661
mattdp <matthew.frank.dupont@gmail.com>
2015-02-05 18:02:01 +0800

    purchasers and factory visits not hardcoded

--- app/controllers/providers_controller.rb
+++ app/controllers/providers_controller.rb
@@ -11,6 +11,8 @@ class ProvidersController < ApplicationController
     @provider = Provider.find_by_name_for_link(params[:name_for_link])
     @comments = @provider.comments
     @tags = @provider.tags
+    @po_names = @comments.select{|c| c.comment_type == "purchase_order"}.map{|c| c.user.lead.lead_contact.first_name_and_team}
+    @fv_names = @comments.select{|c| c.comment_type == "factory_visit"}.map{|c| c.user.lead.lead_contact.first_name_and_team}
     Event.add_event("User",current_user.id,"loaded profile","Provider",@provider.id)
     render layout: "orders_new"
   end
