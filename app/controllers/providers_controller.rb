634559a4bff7ac47d0a6369bb6f2e9d1a03b7ba4
mattdp <matthew.frank.dupont@gmail.com>
2015-02-05 16:44:27 +0800

    tag information real

--- app/controllers/providers_controller.rb
+++ app/controllers/providers_controller.rb
@@ -3,12 +3,14 @@ class ProvidersController < ApplicationController
   
   def index
     @provider_hash = Provider.providers_hash_by_process
+    Event.add_event("User",current_user.id,"loaded index")
     render layout: "orders_new"
   end
 
   def profile
     @provider = Provider.find_by_name_for_link(params[:name_for_link])
     @comments = @provider.comments
+    @tags = @provider.tags
     Event.add_event("User",current_user.id,"loaded profile","Provider",@provider.id)
     render layout: "orders_new"
   end
