49fcb8a043ea0bbc62d22ff55bac8fe172e30f32
James Bond <james.bond.home@gmail.com>
2015-02-05 15:25:14 -0800

    don't load fonts from google for provider pages

--- app/controllers/providers_controller.rb
+++ app/controllers/providers_controller.rb
@@ -4,7 +4,7 @@ class ProvidersController < ApplicationController
   def index
     @provider_hash = Provider.providers_hash_by_process
     Event.add_event("User",current_user.id,"loaded index")
-    render layout: "orders_new"
+    render layout: "provider"
   end
 
   def profile
@@ -14,12 +14,12 @@ class ProvidersController < ApplicationController
     @po_names = @comments.select{|c| c.comment_type == "purchase_order"}.map{|c| c.user.lead.lead_contact.first_name_and_team}
     @fv_names = @comments.select{|c| c.comment_type == "factory_visit"}.map{|c| c.user.lead.lead_contact.first_name_and_team}
     Event.add_event("User",current_user.id,"loaded profile","Provider",@provider.id)
-    render layout: "orders_new"
+    render layout: "provider"
   end
 
   def suggested_edit
     Event.add_event("User",current_user.id,params[:clicked])
-    render layout: "orders_new"
+    render layout: "provider"
   end
 
 end
