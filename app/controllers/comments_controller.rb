2babaed4686807277126b5de1d1bf1287c4119d8
mattdp <matthew.frank.dupont@gmail.com>
2015-02-09 18:45:02 +0800

    working comment submission aside from star rating

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -8,8 +8,24 @@ class CommentsController < ApplicationController
   end
 
   def create
-    #comment_type
-    #user_id
+    @comment = Comment.new(comment_params)
+    @comment.comment_type = "comment"
+    @comment.user_id = current_user.id
+    @comment.save
+
+    saved_ok = @comment.save
+    if saved_ok
+      note = "Saved OK!" 
+    else 
+      note = "Saving problem."
+    end
+
+    provider = Provider.find(params[:provider_id])
+    redirect_to teams_profile_path(provider.name_for_link), notice: note
+  end
+
+  def comment_params
+    params.permit(:overall_score,:payload,:provider_id,:title)
   end
 
 end
