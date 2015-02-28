504b24853eb5cdcc8f8e472bfe54d55da0bb4bdf
James Bond <james.bond.home@gmail.com>
2015-02-28 09:55:48 -0800

    initialize counters

--- db/migrate/20150226154509_add_rating_count_to_comments.rb
+++ db/migrate/20150226154509_add_rating_count_to_comments.rb
@@ -2,5 +2,12 @@ class AddRatingCountToComments < ActiveRecord::Migration
   def change
     add_column :comments, :ratings_count, :integer
     add_column :comments, :helpful_count, :integer
+
+    reversible do |dir|
+      dir.up do
+        Comment.update_all ratings_count: 0, helpful_count: 0
+      end
+    end
+
   end
 end
