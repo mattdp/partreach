7079fa6d4172509278262f83f26cbf3fc67182c4
mattdp <matthew.frank.dupont@gmail.com>
2015-04-07 15:45:48 -0700

    correctly taking in tags and labels for the first example

--- app/models/crawler_synapse_wiki.rb
+++ app/models/crawler_synapse_wiki.rb
@@ -21,8 +21,47 @@ class CrawlerSynapseWiki
   end
 
   #goal of this method is to take an individual source file and create the correct information in synapse's org
-  def self.upload_wiki_page
-    # https://s3.amazonaws.com/temp_for_uploading/1.html (4 files)   
+  #using a bunch of iteration and checking instead of regex, since that's not available via xpath in Rails
+  def self.upload_wiki_pages
+
+    example_urls = ["https://s3.amazonaws.com/temp_for_uploading/1.html",
+      "https://s3.amazonaws.com/temp_for_uploading/2.html",
+      "https://s3.amazonaws.com/temp_for_uploading/3.html",
+      "https://s3.amazonaws.com/temp_for_uploading/4.html"]
+
+    urls = [example_urls[0]] #so everything else works once we have the real ones
+
+    urls.each do |url|
+      page = Nokogiri::HTML(open(url))
+      div_wiki_content = page.css('#content > div.wiki-content')
+      div_wiki_content_headers = div_wiki_content.css("h2")
+      warnings = []
+
+      #DONE bullet points under capabilities, to make into tags
+      tags_from_capabilities = [] 
+      capability_header = div_wiki_content_headers.select{|header| header.attributes["id"].value.include?("Capabilities")}.first
+      capability_list = capability_header.next_element
+      capability_list.css("li").each do |li|
+        tags_from_capabilities << li.text.strip if li.text.present?
+      end
+      warnings >> "No tags from capabilties" if tags_from_capabilities.empty? 
+      
+      #flag if noncompete content 
+      div_wiki_content_headers.select{|header| header.attributes["id"].value.include?("DoNotCompeteWith")}.first
+      #need to check for content and flag it, after question
+        
+      #flag if no contact information, otherwise get it
+      
+      #DONE labels should become tags
+      label_links = page.css(".label-list > .label-container > .label")
+      tags_from_labels = []
+      label_links.each do |label_link|
+        tags_from_labels << label_link.text.strip if label_link.text.present?
+      end
+      warnings >> "No tags from labels" if tags_from_capabilities.empty? 
+
+      return tags_from_capabilities.concat(tags_from_labels), warnings
+    end 
 
   end
 
