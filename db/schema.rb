ca5f27dd44ac9dd99d7c5d084ee6f88f3820cc5a
Robert Martinez <robert.david.martinez@gmail.com>
2015-04-14 15:07:58 -0700

    cutting out all the HAX-related fields

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150413025322) do
+ActiveRecord::Schema.define(version: 20150413232746) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -70,6 +70,9 @@ ActiveRecord::Schema.define(version: 20150413025322) do
     t.integer  "ratings_count",     default: 0
     t.integer  "helpful_count",     default: 0
     t.integer  "purchase_order_id"
+    t.integer  "cost_score",        default: 0
+    t.integer  "quality_score",     default: 0
+    t.integer  "speed_score",       default: 0
   end
 
   create_table "communications", force: true do |t|
