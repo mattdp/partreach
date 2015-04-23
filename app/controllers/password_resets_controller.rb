7689be65b2380efd4505d2ba9e1a742ab953d1cc
James Bond <james.bond.home@gmail.com>
2015-04-23 08:14:33 -0700

    display message after password reset request

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -2,6 +2,7 @@ class PasswordResetsController < ApplicationController
   skip_before_action :allow_staging_access, only: [:new, :create]
   
   def new
+    @message_text = "You will receive an email with instructions on how to reset your password"
     render layout: "provider"
   end
 
@@ -10,8 +11,11 @@ class PasswordResetsController < ApplicationController
     if lead_contact 
       user = lead_contact.contactable.user
       user.send_password_reset if user
+      @message_text = "An email has been sent with password reset instructions."
+    else
+      @message_text = "Sorry, we can't locate that email address in our system."
     end
-    redirect_to teams_signin_url, :notice => "Email sent with password reset instructions."
+    render "new", layout: "provider"
   end
 
 end
