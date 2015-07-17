328c12861566c52c0fb2b16baa49498b591ea7c2
mattdp <matthew.frank.dupont@gmail.com>
2015-07-17 11:11:42 -0700

    working nav header

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -2,8 +2,8 @@ class OrganizationsController < ApplicationController
   before_action :org_access_only
 
   def tags_list
-    @org = current_organization
-    @middle_text = "List of every #{@org.name} Supplier Tag (may take a while to load all rows)"
+    @organization = current_organization
+    @middle_text = "List of every #{@organization.name} Supplier Tag (may take a while to load all rows)"
 
     @tag_details = Rails.cache.fetch("#{current_organization.id}-tag_details-#{Tag.maximum(:updated_at)}-#{Tagging.maximum(:updated_at)}-#{PurchaseOrder.maximum(:updated_at)}") do 
       @org.tag_details
@@ -12,8 +12,8 @@ class OrganizationsController < ApplicationController
 
   def providers_list
     @providers = Provider.where(organization_id: params[:id].to_i)
-    @org = current_organization
-    @middle_text = "List of every #{@org.name} Supplier (may take a while to load all rows)"
+    @organization = current_organization
+    @middle_text = "List of every #{@organization.name} Supplier (may take a while to load all rows)"
   end
 
 end
