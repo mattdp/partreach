48526abb60e6abec25653323f7b0eb5bfedc7938
mattdp <matthew.frank.dupont@gmail.com>
2015-07-17 11:14:17 -0700

    events for new pages

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -2,18 +2,24 @@ class OrganizationsController < ApplicationController
   before_action :org_access_only
 
   def tags_list
+
     @organization = current_organization
     @middle_text = "List of every #{@organization.name} Supplier Tag (may take a while to load all rows)"
 
     @tag_details = Rails.cache.fetch("#{current_organization.id}-tag_details-#{Tag.maximum(:updated_at)}-#{Tagging.maximum(:updated_at)}-#{PurchaseOrder.maximum(:updated_at)}") do 
       @org.tag_details
     end
+
+    Event.add_event("User","#{current_user.id}","viewed tag list")
   end
 
   def providers_list
+
     @providers = Provider.where(organization_id: params[:id].to_i)
     @organization = current_organization
     @middle_text = "List of every #{@organization.name} Supplier (may take a while to load all rows)"
+
+    Event.add_event("User","#{current_user.id}","viewed supplier list")
   end
 
 end
