9409144dae67233c3afc2a65d823fa36371c7a9b
mattdp <matthew.frank.dupont@gmail.com>
2015-07-17 10:03:01 -0700

    partialize nav header for table pages

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -3,6 +3,7 @@ class OrganizationsController < ApplicationController
 
   def tags_list
     @org = current_organization
+    @middle_text = "List of every #{@org.name} Supplier Tag (may take a while to load all rows)"
 
     @tag_details = Rails.cache.fetch("#{current_organization.id}-tag_details-#{Tag.maximum(:updated_at)}-#{Tagging.maximum(:updated_at)}-#{PurchaseOrder.maximum(:updated_at)}") do 
       @org.tag_details
@@ -12,6 +13,7 @@ class OrganizationsController < ApplicationController
   def providers_list
     @providers = Provider.where(organization_id: params[:id].to_i)
     @org = current_organization
+    @middle_text = "List of every #{@org.name} Supplier (may take a while to load all rows)"
   end
 
 end
