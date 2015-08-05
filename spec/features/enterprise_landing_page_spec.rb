1755a0de4e7182175a0922ba534f8771fd2b0da2
mattdp <matthew.frank.dupont@gmail.com>
2015-08-05 15:24:25 -0700

    test succeeding as it should

--- spec/features/enterprise_landing_page_spec.rb
+++ spec/features/enterprise_landing_page_spec.rb
@@ -4,6 +4,6 @@ feature "Enterprise landing page" do
   scenario "should load" do
     visit enterprise_path
 
-    page.should have_content "SupplyBetter for enterprise"
+    page.should have_content "He pays attention to detail"
   end
 end
