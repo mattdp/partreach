12173d3cea1211fe25967ac65ee4b608f90f428c
James Bond <james.bond.home@gmail.com>
2015-09-02 09:14:48 -0700

    remove search placeholder text and search results heading

--- app/assets/javascripts/provider.js
+++ app/assets/javascripts/provider.js
@@ -1,17 +1,7 @@
 $(document).ready(function() {
   // use chosen jquery plugin
-  $('.tag-select').chosen(
-    {
-      placeholder_text_multiple: 'Filter by tags'
-    });
-
   $('.tag-edit').chosen(
     {
       placeholder_text_multiple: 'Click to select tags to add'
-    });
-
-  $('.provider-select').chosen(
-    {
-      placeholder_text_multiple: 'Filter by provider name'
     });
 });
