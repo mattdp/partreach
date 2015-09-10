3467a87a67be20fdccf755b1b478eab876e4ef7d
Robert Martinez <robert.david.martinez@gmail.com>
2015-09-10 13:29:05 -0700

    tweaked placeholder text, nav header partial, and index column sizing

--- app/assets/javascripts/provider.js
+++ app/assets/javascripts/provider.js
@@ -1,6 +1,9 @@
 $(document).ready(function() {
   // use chosen jquery plugin
-  $('.provider-search-terms-select').chosen();
+  $('.provider-search-terms-select').chosen(
+    {
+      placeholder_text_multiple: 'Search for Keywords and Suppliers'
+    });
   $('.tag-edit').chosen(
     {
       placeholder_text_multiple: 'Click to select tags to add'
