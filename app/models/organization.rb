a4059d685c2a137bfd6cdbd87b45393fbcc2a873
mattdp <matthew.frank.dupont@gmail.com>
2015-09-21 20:19:49 -0700

    search string encoder and decoder

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -25,6 +25,34 @@ class Organization < ActiveRecord::Base
   #tags -> which tags are in scope for the organization
   #taggings -> which tags are used for the index page side list
 
+  SEARCH_STRING_SEPARATOR = "-"
+  SEARCH_STRING_MODEL_CHARACTERS = 4
+  SEARCH_STRING_MODEL_HASH = {"Provider"=>"p","Tag"=>"t"}
+
+  #highly promiscous, will encode anything it can across orgs
+  def self.encode_search_string(models)
+    return "" if models.blank?
+    strings = models.map{|m| "#{Organization::SEARCH_STRING_MODEL_HASH[m.class.to_s]}#{m.id}"}
+    strings.join(Organization::SEARCH_STRING_SEPARATOR)
+  end
+
+  #tries to prevent errors and cross-org searching
+  def self.decode_search_string(search_string)
+    answer = []
+    return answer if search_string.blank?
+
+    decoder = Organization::SEARCH_STRING_MODEL_HASH.invert
+    
+    search_string.split(Organization::SEARCH_STRING_SEPARATOR).each do |ss|
+      model_name = decoder[ss[0]]
+      next if model_name.blank?
+      id = ss[1..ss.length]
+      next if id == 0
+      answer << [model_name.constantize.find(id)]
+    end
+    return answer.select{|a| a.organization_id == self.id}
+  end
+
   def common_search_tags(sorted_tags_by_providers)
     minimum_tags_in_list = sorted_tags_by_providers.size
     tags_returning = []
