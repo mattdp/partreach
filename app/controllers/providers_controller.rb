46d9194547ab388457a0301826700f351df19f0b
Matt on iMac <matt@supplybetter.com>
2015-09-22 17:15:50 -0700

    basic search working with neighboring tags

--- app/controllers/providers_controller.rb
+++ app/controllers/providers_controller.rb
@@ -156,7 +156,7 @@ class ProvidersController < ApplicationController
 
       if tags.present?        
         originally_searched_tags = tags
-        if (params[:include_related_tags] == "true" and if tags.size == 1)
+        if (params[:include_related_tags] == "true" and tags.size == 1)
           tag = tags[0]
           Event.add_event("User", current_user.id, "searched one item", "Tag", tag.id)
           @neighboring_tags_by_relationship = tag.immediate_neighboring_tags_by_relationship
