5f07c8682baba069fe7cfbdaa074628fc4a21a6f
Matt on iMac <matt@supplybetter.com>
2015-10-12 16:53:57 -0700

    placeholder text operational

--- app/assets/javascripts/provider.js
+++ app/assets/javascripts/provider.js
@@ -1,14 +1,13 @@
 $(document).ready(function() {
-  // use chosen jquery plugin
-  $('.provider-search-terms-select').chosen(
-    {
-      placeholder_text_multiple: 'Search for Keywords and Suppliers'
-    });
-  $('.tag-edit').chosen(
-    {
-      placeholder_text_multiple: 'Click to select tags to add'
-    });
+  $placeholderForMainSearch = '';
+  $select = $('.selectize');
+
+  if ($select.is(".homepage-search")) {
+    $placeholderForMainSearch = 'Search for Keywords and Suppliers';
+  }
+  
   $('.selectize').selectize({
-    sortField: 'text'
+    sortField: 'text',
+    placeholder: $placeholderForMainSearch
   });
 });
